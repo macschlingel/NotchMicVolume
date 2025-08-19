48d0cb2d13a003f4cd96494f6d48f850f30cef89
Bastian Wölfle <info@schlingel.de>
2025-08-19 11:35:48 +0200

    Initial Commit

---
+++ NotchMicVolume/NotchMicVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotchMicVolumeApp.swift
+//  NotchMicVolume
+//
+//  Created by Bastian Wölfle on 19.08.25.
+//
+
+import SwiftUI
+
+@main
+struct NotchMicVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotchMicVolumeTests/NotchMicVolumeTests.swift
@@ -0,0 +1,17 @@
+//
+//  NotchMicVolumeTests.swift
+//  NotchMicVolumeTests
+//
+//  Created by Bastian Wölfle on 19.08.25.
+//
+
+import Testing
+@testable import NotchMicVolume
+
+struct NotchMicVolumeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
